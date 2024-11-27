5d029e59b5b46a2afb8804bfc6ed06f98abea07a
HIRWA Jean Eric <hirwaminerve25@gmail.com>
2024-11-27 12:55:40 +0200

    Add files via upload

--- src/app/(home)/components/sections/Solution.jsx
+++ src/app/(home)/components/sections/Solution.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const Solution = ({ SolutionSectionData }) => {
   const { description, btn1, btn2 } = SolutionSectionData;
   return (
-    <div className="text-white bg-black flex justify-center py-12 md:py-24">
+    <div className="text-white bg-orange-500 flex justify-center py-12 md:py-24">
       <div className="py-10 px-6 sm:px-10 md:px-20 lg:px-40 max-w-screen-xl text-center flex flex-col gap-5 font-ibm">
           <p className="text-base sm:text-lg md:text-xl">{description}</p>
       </div>
