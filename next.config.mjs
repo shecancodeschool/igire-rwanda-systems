10fd2f46789ca1352d3e187b17fd25bb74df9ac7
HIRWA Jean Eric <hirwaminerve25@gmail.com>
2024-11-27 16:30:24 +0200

    Connected Blog System from SheCanCODE Website

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,51 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
     images: {
-        domains: ['shecancode-website-dev.vercel.app', 'res.cloudinary.com', 'media.licdn.com', 'firebasestorage.googleapis.com'],
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "lh3.googleusercontent.com",
+                port: "",
+                pathname: "/**",
+            },
+            {
+                protocol: "https",
+                hostname: "firebasestorage.googleapis.com",
+                port: "",
+                pathname: "/**",
+            },
+            {
+                protocol: "https",
+                hostname: "res.cloudinary.com",
+                port: "",
+                pathname: "/**",
+            },
+            {
+                protocol: "https",
+                hostname: "avatars.githubusercontent.com",
+                port: "",
+                pathname: "/**",
+            },
+            {
+                protocol: "https",
+                hostname: "i.ibb.co",
+                port: "",
+                pathname: "/**",
+            },
+            {
+                protocol: "https",
+                hostname: "images.unsplash.com",
+                port: "",
+                pathname: "/**",
+            },
+            {
+                protocol: "https",
+                hostname: "shecancode-website-dev.vercel.app",
+                port: "",
+                pathname: "/**",
+            },
+
+        ],
     },
 };
 
